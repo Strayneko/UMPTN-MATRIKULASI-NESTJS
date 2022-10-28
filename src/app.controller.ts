79a2d3eb850123a1c41c59fb10fe80095f2dcfbc
Strayneko <bayurendi10@gmail.com>
2022-10-29 05:12:35 +0700

    :spark: Initial commit on master

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/coba')
+  cobaBang(): string {
+    return this.appService.cobaBang();
+  }
 }
